d26462e48837b8baa5a6fd1c3e030ffd69cffcd2
Sanika Sangappa Kamble <sanikak691@gmail.com>
2025-07-08 15:25:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+//import Counterapp from "./Counterapp"
+//import Weather from "./Weather"
+import Parent from "./Parent";
+function App(){
+  return (
+      <div style={{"text-align":"center"}}>  
+        <Parent/>
+       </div> 
+  )
+}
+export default App;
